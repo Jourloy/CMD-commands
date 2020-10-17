e77f0003b1e35db96d5e95b3cf285ac6bca1f1c7
Igor Shaposhnikov <jourloy@yandex.ru>
2020-10-17 03:31:16 +0300

    Create bat file for get info about commands

---
+++ src/Bat/Info.bat
@@ -0,0 +1,15 @@
+@echo off
+title Info about JOURLOY's commands
+
+echo 1. Disable
+echo    Disable windows update and windows defender
+echo    *Without PYTHON*
+echo 2. Password count [login] [site] [save]
+echo    Generate random password
+echo    login - login for site
+echo    save - if you want save type '1'
+echo    *Need PYTHON*
+echo 3. Speed [full]
+echo    Check your network speed
+echo    full - if you want get more info about type 'full'
+echo    *Need PYTHON*
