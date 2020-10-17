6c57dc19cbf5f7e2d48714f6853fdafd4327af06
Igor Shaposhnikov <jourloy@yandex.ru>
2020-10-17 14:57:32 +0300

    Create install file

---
+++ src/Bat/Install.bat
@@ -0,0 +1,18 @@
+@echo off
+
+title Install JOURLOY's commands
+
+echo -- Made by JOURLOY
+
+echo -- Create folders
+MD C:\CMD-commands
+MD C:\CMD-commands\Bat
+MD C:\CMD-commands\Python
+
+echo -- Downloads files
+curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Bat/Update.bat > C:\CMD-commands\src\Bat\Update.bat
+curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Bat/Setup.bat > C:\CMD-commands\src\Bat\Setup.bat
+curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Python/requirements.txt > C:\CMD-commands\src\Python\requirements.txt
+
+echo -- Run setup batch script
+C:\CMD-commands\src\Bat\Setup.bat
