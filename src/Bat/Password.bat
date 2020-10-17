4a7b50c41433d914e358649dd68cfb3a1a4d20cb
Igor Shaposhnikov <jourloy@yandex.ru>
2020-10-17 14:51:40 +0300

    Add "--help" and "--show"

--- src/Bat/Password.bat
+++ src/Bat/Password.bat
@@ -1,11 +1,34 @@
 @echo off
 
 title Password
-echo --Made by JOURLOY
 
 set count=%1
 set login=%2
 set site=%3
 set save=%4
 
-C:\CMD-commands\src\Python\password.py %1 %2 %3 %4
+if %count%==--help (
+    echo ============================
+    echo [         PASSWORD         ]
+    echo ----------------------------
+    echo Tool for generate random password
+    echo command for launch: Password count [login] [site] [save]
+    echo * count - amount symbols in password
+    echo * [login] - login for site (if you want save password)
+    echo * [site] - address of site (if you want save password)
+    echo command for get saved passwords: Password --show
+    echo command for get help: Password --help
+    echo command for download\update: Download --Password
+    echo ----------------------------
+    echo author: JOURLOY
+    exit
+)
+if %count%==--show (
+    echo -- Made by JOURLOY
+    C:\CMD-commands\src\Python\user.txt
+    exit
+)
+
+echo -- Made by JOURLOY
+C:\CMD-commands\src\Python\password.py %1 %2 %3
+
