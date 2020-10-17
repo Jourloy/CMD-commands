62cbf3e13aef20974a42fe5194e3ae45a8863994
Igor Shaposhnikov <jourloy@yandex.ru>
2020-10-17 14:57:46 +0300

    Remove pip3

--- src/Bat/Setup.bat
+++ src/Bat/Setup.bat
@@ -1,8 +1,5 @@
 @echo off
 title Setup JOURLOY's commands
 
-echo --Install need modules for python
-pip3 install -r ../Python/requirements.txt
-
 echo --Add path
 setx /M path "%path%;C:\CMD-commands\Bat"
