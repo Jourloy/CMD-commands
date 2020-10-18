d5dd98fa284d52661e96a5f5a73714ac0ed0a1c7
Igor Shaposhnikov <jourloy@yandex.ru>
2020-10-18 23:18:38 +0300

    Fix #4

--- src/Bat/Update.bat
+++ src/Bat/Update.bat
@@ -64,7 +64,7 @@ if %param%==--Info (
 if %param%==--Password (
     echo -- Download password tool
     echo -- Author: JOURLOY
-    curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Bat/password.bat > C:\CMD-commands\src\Bat\Password.bat
+    curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Bat/Password.bat > C:\CMD-commands\src\Bat\Password.bat
     curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Python/requirements.txt > C:\CMD-commands\src\Python\requirements.txt
     curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Python/password.py > C:\CMD-commands\src\Python\password.py
     pip3 install -r C:\CMD-commands\src\Python\requirements.txt
@@ -74,42 +74,10 @@ if %param%==--Password (
 if %param%==--Speed (
     echo -- Download speed tool
     echo -- Author: JOURLOY
-    curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Bat/speed.bat > C:\CMD-commands\src\Bat\Speed.bat
+    curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Bat/Speed.bat > C:\CMD-commands\src\Bat\Speed.bat
     curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Python/requirements.txt > C:\CMD-commands\src\Python\requirements.txt
     curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Python/speed.py > C:\CMD-commands\src\Python\speed.py
     pip3 install -r C:\CMD-commands\src\Python\requirements.txt
     echo -- Done
     pause
 )
-if %param%==--all (
-    echo -- Download disable tool
-    echo -- Author: JOURLOY
-    curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Bat/Disable.bat > C:\CMD-commands\src\Bat\Disable.bat
-    echo -- Done
-
-    echo -- Download info tool
-    echo -- Author: JOURLOY
-    curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Bat/Info.bat > C:\CMD-commands\src\Bat\Info.bat
-    echo -- Done
-
-    echo -- Download password tool
-    echo -- Author: JOURLOY
-    curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Bat/Password.bat > C:\CMD-commands\src\Bat\Password.bat
-    curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Python/requirements.txt > C:\CMD-commands\src\Python\requirements.txt
-    curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Python/password.py > C:\CMD-commands\src\Python\password.py
-    pip3 install -r C:\CMD-commands\src\Python\requirements.txt
-    echo -- Done
-
-    echo -- Download speed tool
-    echo -- Author: JOURLOY
-    curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Bat/Speed.bat > C:\CMD-commands\src\Bat\Speed.bat
-    curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Python/requirements.txt > C:\CMD-commands\src\Python\requirements.txt
-    curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Python/speed.py > C:\CMD-commands\src\Python\speed.py
-    pip3 install -r C:\CMD-commands\src\Python\requirements.txt
-    echo -- Done
-
-    echo -- Download setup tool
-    echo -- Author: JOURLOY
-    curl -L https://raw.githubusercontent.com/Jourloy/CMD-commands/master/src/Bat/Setup.bat > C:\CMD-commands\src\Bat\Setup.bat
-    echo -- Done
-) 
