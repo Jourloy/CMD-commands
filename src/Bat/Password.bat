f71e0690e80b4b9e68b8494085ddf22d1f03e2c3
Igor Shaposhnikov <jourloy@yandex.ru>
2020-10-18 23:43:42 +0300

    Fix #1

--- src/Bat/Password.bat
+++ src/Bat/Password.bat
@@ -7,6 +7,8 @@ set login=%2
 set site=%3
 set save=%4
 
+set done=false
+
 if %count%==--help (
     echo ============================
     echo [         PASSWORD         ]
@@ -14,21 +16,26 @@ if %count%==--help (
     echo Tool for generate random password
     echo command for launch: Password count [login] [site] [save]
     echo * count - amount symbols in password
-    echo * [login] - login for site (if you want save password)
-    echo * [site] - address of site (if you want save password)
+    echo * [login] - login for site [if you want save password]
+    echo * [site] - address of site [if you want save password]
     echo command for get saved passwords: Password --show
     echo command for get help: Password --help
     echo command for download\update: Download --Password
     echo ----------------------------
     echo author: JOURLOY
-    exit
+    set done=true
+    pause
 )
 if %count%==--show (
     echo -- Made by JOURLOY
     C:\CMD-commands\src\Python\user.txt
-    exit
+    set done=true
 )
-
-echo -- Made by JOURLOY
-C:\CMD-commands\src\Python\password.py %1 %2 %3
-
+if not %done%==true (
+    echo ============================
+    echo [         PASSWORD         ]
+    echo ----------------------------
+    python C:\CMD-commands\src\Python\password.py %1 %2 %3
+    echo ----------------------------
+    pause
+)
