277a3aea85572295fe1cde455f8959b4174d75db
Igor Shaposhnikov <jourloy@yandex.ru>
2020-10-20 23:55:33 +0300

    Fix #8

--- src/Bat/WinDefender.bat
+++ src/Bat/WinDefender.bat
@@ -1,46 +1,22 @@
 @echo off
 
-title Disable Windows Update
+title Disable Windows Defender
 
-set param=%1
-set admin=%2
+echo ============================
+echo [        WinDEFENDER       ] 
+echo ----------------------------
+echo -- Generate disable defender reg file
+set "FF="%ProgramFiles%\DisableDefender.reg""
+echo Windows Registry Editor Version 5.00>>%FF%
+echo [HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender]>>%FF%
+echo "DisableAntiSpyware"=dword:00000001>>%FF%
+echo [HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection]>>%FF%
+echo "DisableBehaviorMonitoring"=dword:00000001>>%FF%
+echo "DisableBehaviorMonitoring"=dword:00000001>>%FF%
+echo "DisableScanOnRealtimeEnable"=dword:00000001>>%FF%
 
-if %param%==--help (
-    echo ============================
-    echo [        WinDEFENDER       ] 
-    echo ----------------------------
-    echo Tool for disable windows update and windows defender
-    echo author: JOURLOY
-    echo command for launch: WinDefender run
-    echo command for get help: WinDefender --help
-    echo command for download\update: Download --WinDefender
-)
+echo -- Run disable defender reg file
+regedit.exe /S "%ProgramFiles%\DisableDefender.reg"
+del /Q "%ProgramFiles%\DisableDefender.reg"
 
-if %admin%==adm (
-    if %param%==run (
-        echo ============================
-        echo [        WinDEFENDER       ] 
-        echo ----------------------------
-        set /p run=Will be system reboot. Are you ready? (yes/no): 
-        if %run%==yes (
-            echo -- Generate disable defender reg file
-            set "FF="%ProgramFiles%\DisableDefender.reg""
-            echo Windows Registry Editor Version 5.00>>%FF%
-            echo [HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender]>>%FF%
-            echo "DisableAntiSpyware"=dword:00000001>>%FF%
-            echo [HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection]>>%FF%
-            echo "DisableBehaviorMonitoring"=dword:00000001>>%FF%
-            echo "DisableBehaviorMonitoring"=dword:00000001>>%FF%
-            echo "DisableScanOnRealtimeEnable"=dword:00000001>>%FF%
-
-            echo -- Run disable defender reg file
-            regedit.exe /S "%ProgramFiles%\DisableDefender.reg"
-            del /Q "%ProgramFiles%\DisableDefender.reg"
-
-            shutdown /r
-        )
-    )
-)
-else (
-    powershell.exe Start-Process "C:\CMD-commands\src\Bat\WinDisable.bat --%param% adm" -Verb runAs
-)
+shutdown /r
